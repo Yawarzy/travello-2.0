61e7bf7543d7202a32b57de3b65c037d9c62f497
Yawarzy <yawarbhatzy@gmail.com>
2022-07-01 12:31:53 +0530

    Finished slider component

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+////////////////////// Mobile Navigation
 const nav = document.querySelector(".nav");
 const hamburger = document.querySelector(".nav__hamburger");
 const navLinks = document.querySelector(".nav__list");
@@ -13,9 +14,76 @@ const toggleMenu = function () {
   hamburger.classList.toggle("close");
 };
 
-// Eventlisteners
+// eventlisteners
 window.addEventListener("DOMContentLoaded", setNavListPosition);
 
 window.addEventListener("resize", setNavListPosition);
 
 hamburger.addEventListener("click", toggleMenu);
+
+//////////////// Hero Slider
+const slider = document.querySelector(".hero__slides");
+const slides = document.querySelectorAll(".hero__slide");
+const pagination = document.querySelector(".pagination");
+const circles = document.querySelectorAll(".circle");
+
+let currentSlide = 0;
+let totalSlides = slides.length;
+
+// btns
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+// Functions
+const goToSlide = function (curSlide) {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+  });
+};
+
+const activateCircle = function (curSlide) {
+  circles.forEach((circle) => {
+    circle.classList.remove("circle--active");
+  });
+
+  document
+    .querySelector(`.circle[data-slide="${curSlide}"]`)
+    .classList.add("circle--active");
+};
+
+// set first slide
+goToSlide(currentSlide);
+activateCircle(currentSlide);
+
+// Next slide
+nextBtn.addEventListener("click", () => {
+  if (currentSlide === totalSlides - 1) currentSlide = 0;
+  else currentSlide++;
+  goToSlide(currentSlide);
+  activateCircle(currentSlide);
+});
+
+// Prev slide
+prevBtn.addEventListener("click", () => {
+  if (currentSlide === 0) currentSlide = totalSlides - 1;
+  else currentSlide--;
+  goToSlide(currentSlide);
+  activateCircle(currentSlide);
+});
+
+// Pagination
+pagination.addEventListener("click", (e) => {
+  if (e.target.classList.contains("circle")) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateCircle(slide);
+  }
+});
+
+// Slideshow
+setInterval(() => {
+  if (currentSlide === totalSlides - 1) currentSlide = 0;
+  else currentSlide++;
+  goToSlide(currentSlide);
+  activateCircle(currentSlide);
+}, 5000);
